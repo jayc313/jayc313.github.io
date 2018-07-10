3fa74a782ff1a0a4ab26242b5f165e8bb649fa62
jayc313 <jay.cutler313@gmail.com>
2018-07-10 22:32:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function($j){
 
     var reedAPI = 'a1133cb2-dff9-4582-8408-e16317a553c3',
-    reedURL = 'https://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london&employerid=123&distancefromlocation=15';
+    reedURL = 'https://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london';
 
 
     $j.ajax({url: reedURL, username: reedAPI,
