f9c2f36aae554870926d2cfd6aa943f5a778b802
jayc313 <jay.cutler313@gmail.com>
2018-07-10 22:28:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 (function($j){
 
     var reedAPI = 'a1133cb2-dff9-4582-8408-e16317a553c3',
     reedURL = 'https://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london&employerid=123&distancefromlocation=15';
 
 
-    $j.ajax({url: reedURL, headers: {
-        "Authorization": "Basic " + btoa(reedAPI + ":" + '')
+    $j.ajax({url: reedURL,    beforeSend: function (xhr) {
+
+        xhr.setRequestHeader('Authorization', 'Basic ' + btoa(reedAPI + ":" + ''));
+
       }, success: function(result){
         console.log(result);
     }});
